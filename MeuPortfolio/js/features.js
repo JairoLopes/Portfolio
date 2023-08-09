c785e89fb3a2a03effbeac53129e8751c20ec97d
Ereb0 <73255336+JairoLopes@users.noreply.github.com>
2023-08-09 19:02:12 -0300

    up

---
+++ MeuPortfolio/js/features.js
@@ -0,0 +1,11 @@
+
+function clickMenu() {
+    if (menuHeaderClicavel.style.display == "block") {
+
+        menuHeaderClicavel.style.display = "none"
+
+    } else {
+
+        menuHeaderClicavel.style.display = "block"
+    }
+}
